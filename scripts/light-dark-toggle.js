4418fcac9ad9b936ae296460e522ab18a1c7f73d
Wes Madaj <wtmadaj@gmail.com>
2021-08-07 22:26:38 -0400

    toggle switch, first attempt at light/dark toggle

---
+++ scripts/light-dark-toggle.js
@@ -0,0 +1,9 @@
+function toggle() {
+    // var element = document.body;
+    // element.classList.toggle("dark-mode");
+    // document.getElementById("body").style.cssText="background-color: white";
+    document.body.style.cssText = 'background: white;';
+}
+//https://www.simpleweblearning.com/how-to-create-light-dark-mode-toggle-with-css-and-javascript/
+//https://codepen.io/chen1223/pen/PobxwMj
+//https://www.w3schools.com/howto/howto_css_switch.asp
